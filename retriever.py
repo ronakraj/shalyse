ae81927e50c3e3a5cd53e74e3237933d8ed19f75
Ronakraj <ronakraj.gosalia@uqconnect.edu.au>
2021-12-12 11:42:17 +1100

    Update retriever.py
    Type setting retriever.py for ease of reading.

--- retriever.py
+++ retriever.py
@@ -14,10 +14,18 @@ class Retriever:
         self.period = period
         self.csv_fp = self.ticker + ".csv"
         self.json_fp = self.ticker + ".csv"
-        self.data, self.info = self.get_ticker_data(\
+        self.data, self.info = self._get_ticker_data(\
             self.ticker, self.period, self.csv_fp, self.json_fp)
 
-    def get_ticker_data(self, ticker, period, csv_fp, json_fp):
+    def _get_ticker_data(self, ticker: str, period: str, csv_fp: str, \
+            json_fp: str) -> dict:
+        """
+        Yields information and all data from Yahoo Finance server for 
+        provided ticker ID and stores these to json_fp and csv_fp files 
+        respectively. Also returns both in dictionary format for use during
+        runtime.
+        """
+        
         # Check if data for ticker exists already
         if not os.path.isfile(ticker + ".csv"):
             # Retrieve price data for specified time period 
